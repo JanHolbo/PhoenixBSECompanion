40d33d81f7d5cbb7a6e0144ff6769c87f17b5274
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2019-02-12 23:41:28 +0100

    Corrected missing preceeding 0's in decimal numbers

--- app/src/main/java/dk/kaddu/phoenixbsecompanion/MainActivity.java
+++ app/src/main/java/dk/kaddu/phoenixbsecompanion/MainActivity.java
@@ -1,13 +1,23 @@
 package dk.kaddu.phoenixbsecompanion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "dk.kaddu.phoenixbsecompanion.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void viewPreferences(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
