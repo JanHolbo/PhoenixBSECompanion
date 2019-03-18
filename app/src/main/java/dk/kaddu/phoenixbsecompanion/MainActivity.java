1a18661b5f854b2fab5dfe5c0cdd20289a9e53b7
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2019-03-18 19:37:10 +0100

    Moved network code to NetworkFragtment so that network activity is off the main UI tread
    Signed-off-by: Jan Oscar Holbo Rasmussen <jan@kaddu.dk>

--- app/src/main/java/dk/kaddu/phoenixbsecompanion/MainActivity.java
+++ app/src/main/java/dk/kaddu/phoenixbsecompanion/MainActivity.java
@@ -1,37 +1,98 @@
 package dk.kaddu.phoenixbsecompanion;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements DownloadCallback {
     public static final String EXTRA_MESSAGE = "dk.kaddu.phoenixbsecompanion.MESSAGE";
     private static final String LOG_TAG = "MainActivity";
 
-    private String star_date="Not Available";
-    private String status="Not Available";
+    // Keep a reference to the NetworkFragment, which owns the AsyncTask object
+    // that is used to execute network ops.
+    private NetworkFragment networkFragment;
+
+    // Boolean telling us whether a download is in progress, so we don't trigger overlapping
+    // downloads with consecutive button clicks.
+    private boolean downloading = false;
+
+    private void startDownload() {
+        if (!downloading && networkFragment != null) {
+            // Execute the async download.
+            networkFragment.startDownload();
+            downloading = true;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO add whatever is needed on onCreate()
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        updateGameStatus(this.findViewById(R.id.button_updateStatus));
+        // TODO do we need to instantiate/reconnect to the NetworkFragment on UI redraw?
+//        networkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://www.google.com");
+
+//        updateGameStatus(this.findViewById(R.id.button_updateStatus));
+    }
+
+//    @Override
+    public void updateFromDownload(String result) {
+        // Update your UI here based on result of download.
+    }
+
+    @Override
+    public void updateFromDownload(Object result) {
+        // Update your UI here based on result of download.
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo;
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch(progressCode) {
+            // You can add UI behavior for progress updates here.
+            case Progress.ERROR:
+                // TODO onProgressUpdate() add code in case of Progress.ERROR
+                break;
+            case Progress.CONNECT_SUCCESS:
+                // TODO onProgressUpdate() add code in case of Progress.CONNECT_SUCCESS
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                // TODO onProgressUpdate() add code in case of Progress.GET_INPUT_STREAM_SUCCESS
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                // TODO onProgressUpdate() add code in case of Progress.PROCESS_INPUT_STREAM_IN_PROGRESS
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                // TODO onProgressUpdate() add code in case of Progress.PROCESS_INPUT_STREAM_SUCCESS
+                break;
+        }
+    }
+
+    @Override
+    public void finishDownloading() {
+        downloading = false;
+        if (networkFragment != null) {
+            networkFragment.cancelDownload();
+        }
     }
 
     /** Called when the user taps the Update Game Status button */
@@ -39,16 +100,23 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences prefs =
                 PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 // TODO replace the hardcoded xml query string with the preferences driven uriBuilder string
-//        String xmlQueryUriString = "http://www.phoenixbse.co.uk/?a=xml&uid=197&code=3c3a6f899bd43b329152574c15190a27&sa=game_status";
+        String xmlQueryUrlString = "https://www.phoenixbse.co.uk/?a=xml&uid=197&code=3c3a6f899bd43b329152574c15190a27&sa=game_status";
+/*
 // TODO replace the hardcoded uriBuilder string with the preferences driven uriBuilder string
-        String xmlQueryUriString = uriBuilder(
+        String xmlQueryUrlString = uriBuilder(
                 prefs.getString("nexus_uri", "www.phoenixbse.co.uk"),
                 prefs.getString("nexus_uid", "1"),
                 prefs.getString("nexus_code", "22d9b2c0316adab0f9104571c7ed8eb0"));
-        Log.d(LOG_TAG,"xmlQueryUriString = " + xmlQueryUriString);
+*/        Log.d(LOG_TAG,"xmlQueryUrlString = " + xmlQueryUrlString);
 
+        networkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), xmlQueryUrlString);
+
+        startDownload();
+
+// TODO remove old network code that executed on the main UI thread
+/* Old network code that executed on the main UI thread which is no longer allowed - TO BE REMOVED
         try {
-            URL xmlQueryUrl = new URL(xmlQueryUriString);
+            URL xmlQueryUrl = new URL(xmlQueryUrlString);
 
             InputStream gameStatusInputStream = null;
             try {
@@ -125,15 +193,19 @@ public class MainActivity extends AppCompatActivity {
             Log.e(LOG_TAG,"updateGameStatus: other Exception = " + e.toString());
 
         }
+Old network code that executed on the main UI thread which is no longer allowed - TO BE REMOVED */
 
-
-        TextView stardateTextView = (TextView) findViewById(R.id.textView_current_stardate);
+// TODO replace old UI update code with code that reacts through the DownloadCallback code
+/* Old code that updates the UI upon having done the XML parsing - TO BE REMOVED
+        TextView stardateTextView = findViewById(R.id.textView_current_stardate);
         stardateTextView.setText(star_date);
-        TextView statusTextView = (TextView) findViewById(R.id.textView_current_gameStatus);
+        TextView statusTextView = findViewById(R.id.textView_current_gameStatus);
         statusTextView.setText(status);
+
+        code that updates the UI upon having done the XML parsing - TO BE REMOVED */
     }
 
-    private void processParsing (XmlPullParser parser) throws IOException, XmlPullParserException {
+    private void processParsing (XmlPullParser parser) {
         // TODO add XML parsing code for the Game Status XML response
 
     }
