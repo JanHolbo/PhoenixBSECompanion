a2aebfd0388264c872d60137013b2f8b2c4136f3
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2019-03-30 10:19:26 +0100

    Changed gameStatusTextView to a button.  - With time this will lead to an Activity screen showing statistics on completion time of the different daily stages

--- app/src/main/java/dk/kaddu/phoenixbsecompanion/data/GameStatusList.kt
+++ app/src/main/java/dk/kaddu/phoenixbsecompanion/data/GameStatusList.kt
@@ -3,5 +3,13 @@ package dk.kaddu.phoenixbsecompanion.data
 data class GameStatusList (val gameStatus: List<GameStatus>)
 
 data class GameStatus (
+        val current_date: Int?,
+        val year_start: Int?,
+        val turns_downloaded: Int?,
+        val turns_processed: Int?,
+        val turns_uploaded: Int?,
+        val emails_sent: Int?,
+        val specials_processed: Int?,
+        val day_finished: Int?,
         val star_date: String?,
         val status: String?)
