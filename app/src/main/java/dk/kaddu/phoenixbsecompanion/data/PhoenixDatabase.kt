bfba9e7c465391208535e732694a76c740290240
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2019-04-06 17:47:56 +0200

    Items - related development branched to develop-room-items  + related code purged from this branch to focus on the delivery of the 0.30 milestone
    TODO for 0.3 Beta:
    Update UI
     + replace GameStatus button with a TextView with 10 lines. This will serve as the information vehicle to the user
     + info mockup:
     |  App name + versionName
     |  About text
     |  %s Game Status records loaded
     |  Star Date : %s
     |  Ready
     + if the day has started
     |  Turns downloaded at %s
     |  Turns processed expected at %s
     + when GameStatus changes (triggered through LiveData:
     |  Turns processed at %s
     |  Turns uploaded expected at %s
     + when writing timed events convert the TimeStamp to time String
    Add a Timer to run the task of updating the GameStatus
     + Do not run on Saturdays & Sundays
     + update once an hour (this has to be reviewed)

--- app/src/main/java/dk/kaddu/phoenixbsecompanion/data/PhoenixDatabase.kt
+++ app/src/main/java/dk/kaddu/phoenixbsecompanion/data/PhoenixDatabase.kt
@@ -45,14 +45,6 @@ public abstract class PhoenixDatabase : RoomDatabase() {
         ) : RoomDatabase.Callback() {
 
         fun populateDatabase (gameStatusDao: GameStatusDao) {
-/*
-            var gameStatus = GameStatus (4504, 4400, 0, 0, 0, 0, 0, 0, "219.13.5", "Upload Specials")
-            gameStatusDao.insert(gameStatus)
-            gameStatus = GameStatus (4506, 4400, 1554184142, 1554195721, 1554197460, 1554200828, 1554215821, 1554215983, "219.14.2", "Upload Specials")
-            gameStatusDao.insert(gameStatus)
-            gameStatus = GameStatus (4507, 4400, 1554270610, 1554278628, 1554279910, 1554281006, 1554300436, 1554300605, "219.14.3", "Upload Specials")
-            gameStatusDao.insert(gameStatus)
-*/
         }
 
         override fun onOpen(db: SupportSQLiteDatabase) {
